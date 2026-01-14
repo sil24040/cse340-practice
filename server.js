2c7da1eac6cabf4b894d22138fd860be3364955d
sbd488 <sil24040@byui.edu>
2026-01-13 22:38:25 -0700

    Add routes and static pages for home/about/products

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
+// Imports
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
 
+/**
+ * Declare Important Variables
+ */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 3000;
+
+/**
+ * Setup Express Server
+ */
 const app = express();
-const name = process.env.NAME;
 
+/**
+ * Configure Express middleware
+ */
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+/**
+ * Routes
+ */
 app.get("/", (req, res) => {
-  res.send(`Hello, ${name}!`);
+  res.sendFile(path.join(__dirname, "src/views/home.html"));
 });
 
-const PORT = 3000;
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/views/about.html"));
+});
 
+app.get("/products", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/views/products.html"));
+});
+
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
